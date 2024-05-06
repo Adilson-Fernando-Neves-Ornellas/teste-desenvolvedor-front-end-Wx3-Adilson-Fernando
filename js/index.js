a3a101c2a08ef137b404bc98aa5d795135489514
Adilson Fernando Neves Ornellas <ornellas00@hotmail.com>
2024-05-06 18:21:59 -0300

    Feat: slick banner de cima

--- js/index.js
+++ js/index.js
@@ -7,3 +7,24 @@ function closeMenu() {
     var menu = document.getElementById("menu_hamburguer_header");
     menu.classList.remove("active");
 }
+
+// slick 01 
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-slide');
+
+function showSlide(index) {
+    if (index >= 0 && index < slides.length) {
+        currentSlide = index;
+        slides.forEach((slide, i) => {
+            if (i === index) {
+                slide.style.display = 'block';
+            } else {
+                slide.style.display = 'none';
+            }
+        });
+    }
+}
+
+showSlide(currentSlide);
+
+// slick 02
