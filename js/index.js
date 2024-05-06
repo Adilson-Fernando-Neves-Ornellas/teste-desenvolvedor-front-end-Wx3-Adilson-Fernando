a56d3fbe2f47befe2bd6e644d79c5ca3d1fdc23e
Adilson Fernando Neves Ornellas <ornellas00@hotmail.com>
2024-05-06 14:02:14 -0300

    feat: header responsivo com modal quando mobile

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var menu = document.getElementById("menu_hamburguer_header");
+    menu.classList.toggle("active");
+}
+
+function closeMenu() {
+    var menu = document.getElementById("menu_hamburguer_header");
+    menu.classList.remove("active");
+}
