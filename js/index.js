7f20752fe5c4e5e9f8c3894304b35214b9ccc4df
Adilson Fernando Neves Ornellas <ornellas00@hotmail.com>
2024-05-06 20:07:22 -0300

    feat: slick 03 destaques

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ function closeMenu() {
     menu.classList.remove("active");
 }
 
-// slick 01 
+// slick 01 ----------------------------------------------------------------------------------------------------
 let currentSlide = 0;
 const slides = document.querySelectorAll('.carousel-slide');
 
@@ -27,7 +27,7 @@ function showSlide(index) {
 
 showSlide(currentSlide);
 
-// slick 02
+// slick 02-----------------------------------------------------------------------------------------------------
 let currentIndex = 0;
 const items = document.querySelectorAll('.carousel-item');
 const dotsContainer = document.querySelector('.carousel-dots');
@@ -73,3 +73,50 @@ function prevSlide() {
 
 // Exibir o primeiro slide inicialmente
 showSlideDois(currentIndex);
+
+// slick 03-----------------------------------------------------------------------------------------------------
+let currentIndexTres = 0;
+const itemsSlickTres = document.querySelectorAll('.carousel-item-tres');
+const dotsContainerTres = document.querySelector('.carousel-dots-tres');
+
+// Criar os indicadores de posição (bolinhas)
+for (let i = 0; i < itemsSlickTres.length; i++) {
+    const dot = document.createElement('div');
+    dot.classList.add('carousel-dot-tres');
+    if (i === currentIndexTres) {
+        dot.classList.add('active');
+    }
+    dot.setAttribute('onclick', `goToSlideTres(${i})`);
+    dotsContainerTres.appendChild(dot);
+}
+
+function showSlideTres(index) {
+    itemsSlickTres.forEach((item, i) => {
+        const offset = index * -100;
+        item.style.transform = `translateX(${offset}%)`;
+    });
+
+    // Atualizar o indicador de posição (bolinhas)
+    const dots = document.querySelectorAll('.carousel-dot-tres');
+    dots.forEach((dot, i) => {
+        dot.classList.toggle('active', i === index);
+    });
+}
+
+function goToSlideTres(index) {
+    currentIndexTres = index;
+    showSlideTres(currentIndexTres);
+}
+
+function nextSlideTres() {
+    currentIndexTres = (currentIndexTres + 1) % itemsSlickTres.length;
+    showSlideTres(currentIndexTres);
+}
+
+function prevSlideTres() {
+    currentIndexTres = (currentIndexTres - 1 + itemsSlickTres.length) % itemsSlickTres.length;
+    showSlideTres(currentIndexTres);
+}
+
+// Exibir o primeiro slide inicialmente
+showSlideTres(currentIndexTres);
